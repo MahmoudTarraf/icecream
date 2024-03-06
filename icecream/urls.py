44c062eca81b5aab198a252c486a5ca2695335cc
Mahmoud Tarraf <mahmoudtarraf77@gmail.com>
2024-03-06 08:16:38 -0500

    third commit

--- icecream/urls.py
+++ icecream/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('api.urls')),
-    # path('users/',include('users.urls')),
+    path('users/',include('users.urls')),
     # path('users/',include('django.contrib.auth.urls')),
 ]
